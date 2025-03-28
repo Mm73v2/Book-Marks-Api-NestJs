efb1360b45848607c9dd857c4cba0d6789dc1e55
= <=>
2025-03-28 22:58:25 +0200

    handled duplicate email exception

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,30 +1,40 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
 import { AuthDto } from "src/dto";
 import { PrismaService } from "src/prisma/prisma.service";
 import { hash } from "argon2";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 @Injectable()
 class AuthService {
   constructor(private prisma: PrismaService) {}
 
   async signup(dto: AuthDto) {
-    const hashedPassword = await hash(dto.password);
+    try {
+      const hashedPassword = await hash(dto.password);
 
-    const user = await this.prisma.user.create({
-      data: {
-        email: dto.email,
-        password: hashedPassword,
-      },
-      select: {
-        id: true,
-        createdAt: true,
-        email: true,
-        updatedAt: true,
-        firstName: true,
-        lastName: true,
-      },
-    });
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          password: hashedPassword,
+        },
+        select: {
+          id: true,
+          createdAt: true,
+          email: true,
+          updatedAt: true,
+          firstName: true,
+          lastName: true,
+        },
+      });
 
-    return user;
+      return user;
+    } catch (error: any) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === "P2002") {
+          throw new ForbiddenException("This email is already in use");
+        }
+      }
+      throw error;
+    }
   }
 
   login() {}
